e75ee337012dec150ee9bfd8d4bb9fab04fd49b5
Shray bansal <bansalshray@gmail.com>
2016-10-09 13:55:55 -0700

    added merge quick insertion tests

--- test/sorting.js
+++ test/sorting.js
@@ -11,15 +11,29 @@ describe('Bubble Sort', function(){
 	});
 });
 
+xdescribe('Merge Sort', function(){
+	it('Basic Case', function(){
+		expect(Sort.merge([1,2,3,4,6,5])).to.eql([1,2,3,4,5,6]);
+	});
+	it('Larger case', function(){
+		expect(Sort.merge([5,1,36,34,14,23])).to.eql([1,5,14,23,34,36]);
+	});
+});
 
+xdescribe('Quick Sort', function(){
+	it('Basic Case', function(){
+		expect(Sort.quick([1,2,3,4,6,5])).to.eql([1,2,3,4,5,6]);
+	});
+	it('Larger case', function(){
+		expect(Sort.quick([5,1,36,34,14,23])).to.eql([1,5,14,23,34,36]);
+	});
+});
 
-
-
-
-
-
-
-
-
-
-
+xdescribe('Insertion Sort', function(){
+	it('Basic Case', function(){
+		expect(Sort.insert([1,2,3,4,6,5])).to.eql([1,2,3,4,5,6]);
+	});
+	it('Larger case', function(){
+		expect(Sort.insert([5,1,36,34,14,23])).to.eql([1,5,14,23,34,36]);
+	});
+});
